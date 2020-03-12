bce9db0f912c05b382bc64e772d021d989f10385
SAMCHEE <jibitow@gmail.com>
2020-03-12 16:33:04 +0900

    comment main() in test.c
    fix error 'dupulicate main function'

--- libs/driver/libetherdream/test.c
+++ libs/driver/libetherdream/test.c
@@ -93,49 +93,49 @@ void fill_circle(float phase, int mode) {
 */
 	}
 }
-
-int main(int argc, char **argv) {
-	etherdream_lib_start();
-
-	/* Sleep for a bit over a second, to ensure that we see broadcasts
-	 * from all available DACs. */
-	usleep(1200000);
-
-	int cc = etherdream_dac_count();
-	if (!cc) {
-		printf("No DACs found.\n");
-		return 0;
-	}
-
-	int mode;
-	if (argc > 1)
-		mode = atoi(argv[1]);
-	else
-		mode = 0;
-
-	int i;
-	for (i = 0; i < cc; i++) {
-		printf("%d: Ether Dream %06lx\n", i,
-			etherdream_get_id(etherdream_get(i)));
-	}
-
-	struct etherdream *d = etherdream_get(0);
-
-	printf("Connecting...\n");
-	if (etherdream_connect(d) < 0)
-		return 1;
-
-	i = 0;
-	while (1) {
-		fill_circle((float)i / 50, mode);
-		int res = etherdream_write(d, circle, CIRCLE_POINTS, 30000, 1);
-		if (res != 0) {
-			printf("write %d\n", res);
-		}
-		etherdream_wait_for_ready(d);
-		i++;
-	}
-
-	printf("done\n");
-	return 0;
-}
+//
+//int main(int argc, char **argv) {
+//	etherdream_lib_start();
+//
+//	/* Sleep for a bit over a second, to ensure that we see broadcasts
+//	 * from all available DACs. */
+//	usleep(1200000);
+//
+//	int cc = etherdream_dac_count();
+//	if (!cc) {
+//		printf("No DACs found.\n");
+//		return 0;
+//	}
+//
+//	int mode;
+//	if (argc > 1)
+//		mode = atoi(argv[1]);
+//	else
+//		mode = 0;
+//
+//	int i;
+//	for (i = 0; i < cc; i++) {
+//		printf("%d: Ether Dream %06lx\n", i,
+//			etherdream_get_id(etherdream_get(i)));
+//	}
+//
+//	struct etherdream *d = etherdream_get(0);
+//
+//	printf("Connecting...\n");
+//	if (etherdream_connect(d) < 0)
+//		return 1;
+//
+//	i = 0;
+//	while (1) {
+//		fill_circle((float)i / 50, mode);
+//		int res = etherdream_write(d, circle, CIRCLE_POINTS, 30000, 1);
+//		if (res != 0) {
+//			printf("write %d\n", res);
+//		}
+//		etherdream_wait_for_ready(d);
+//		i++;
+//	}
+//
+//	printf("done\n");
+//	return 0;
+//}
